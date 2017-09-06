48b5e284f8d7e0631d16b8b7f7f0d73a5b781f4a
imaxct <imaxct@foxmail.com>
2017-09-07 00:25:31 +0800

    添加前端代码.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,8 +1,15 @@
 $(function () {
     var $head = $('#head'), $list = $('#list');
-    $head.display({displayed: function () {$('#list').find('>a[data-display-auto]').trigger('click');}});
+    $head.display({displayed: function () {$('#list').find('a[data-display-auto]').trigger('click');}});
     $list.display({
         selector: 'a', activeClass: 'active', target: '#con', trigger: 'click', targetZIndex: 'none',
         name: 'list', show: function () {$('#loading').show();}, shown: function () {$('#loading').hide();}
     });
 });
+
+var Tip = {
+    _dis: new $.Display({display: 'messager'}),
+    show: function(msg, succ) {
+        Tip._dis.show({content: msg?msg:'操作失败', type: succ?'success':'danger'})
+    }
+};
